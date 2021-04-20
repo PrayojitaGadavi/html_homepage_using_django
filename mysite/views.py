ef57ecef1ca1875477dd5d804538dda4e7116d45
PrayojitaGadavi <prayugadavi@gmail.com>
2021-04-20 08:01:22 -0700

    first commit

---
+++ mysite/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def map(request):
+    return render(request, 'map.html')
+
+def technology(request):
+    return render(request, 'technology.html')
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
